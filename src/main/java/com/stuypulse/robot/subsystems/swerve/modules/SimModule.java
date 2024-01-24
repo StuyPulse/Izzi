5b28fbacc865712eddb8c08a2a267b516bc4f84c
Richie_Xue <rxue40@stuy.edu>
2024-01-23 23:18:24 -0500

    Refactor identifyPositionVelocitySystem into PositionVelocitySystem.java

--- src/main/java/com/stuypulse/robot/subsystems/swerve/modules/SimModule.java
+++ src/main/java/com/stuypulse/robot/subsystems/swerve/modules/SimModule.java
@@ -1,9 +1,8 @@
-package com.stuypulse.robot.subsystems.swerve.modules;
-
-import com.stuypulse.robot.constants.Settings;
+package com.stuypulse.robot.subsystems.swerve.modules;import com.stuypulse.robot.constants.Settings;
 import com.stuypulse.robot.constants.Settings.Swerve;
 import com.stuypulse.robot.constants.Settings.Swerve.Controller.Drive;
 import com.stuypulse.robot.constants.Settings.Swerve.Controller.Turn;
+import com.stuypulse.robot.util.PositionVelocitySystem;
 import com.stuypulse.stuylib.control.Controller;
 import com.stuypulse.stuylib.control.angle.AngleController;
 import com.stuypulse.stuylib.control.angle.feedback.AnglePIDController;
@@ -12,15 +11,13 @@ import com.stuypulse.stuylib.control.feedforward.MotorFeedforward;
 import com.stuypulse.stuylib.math.Angle;
 import com.stuypulse.stuylib.streams.angles.filters.ARateLimit;
 
-import edu.wpi.first.math.MatBuilder;
-import edu.wpi.first.math.Nat;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N2;
-import edu.wpi.first.math.system.LinearSystem;
+
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.BatterySim;
 import edu.wpi.first.wpilibj.simulation.LinearSystemSim;
@@ -28,37 +25,6 @@ import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SimModule extends AbstractSwerveModule {
-    /**
-     * Create a state-space model for a 1 DOF position system from its kV (volts/(unit/sec)) and kA (volts/(unit/sec²). 
-     * <li> States: [position, velocity]ᵀ 
-     * <li> Inputs  [voltage]
-     * <li> Outputs [position, velocity]
-     * 
-     * <p>The distance unit MUST either meters or radians.
-     * <p>The parameters provided by the user are from this feedforward model:
-     * <p>u = K_v v + K_a a
-     *
-     * @param kV The velocity gain, in volts/(unit/sec)
-     * @param kA The acceleration gain, in volts/(unit/sec²)
-     * @return A LinearSystem representing the given characterized constants.
-     * @throws IllegalArgumentException if kV &lt;= 0 or kA &lt;= 0.
-     */
-    private static LinearSystem<N2, N1, N2> identifyVelocityPositionSystem(double kV, double kA) {
-        if (kV <= 0.0) {
-            throw new IllegalArgumentException("kV must greater than zero");
-        }
-        if (kA <= 0.0) {
-            throw new IllegalArgumentException("kA must be greater than zero");
-        }
-
-        return new LinearSystem<N2, N1, N2> (
-            MatBuilder.fill(Nat.N2(), Nat.N2(), 0.0, 1.0, 0.0, -kV / kA),
-            MatBuilder.fill(Nat.N2(), Nat.N1(), 0.0, 1.0 / kA),
-            MatBuilder.fill(Nat.N2(), Nat.N2(), 1.0, 0.0, 0.0, 1.0),
-            MatBuilder.fill(Nat.N2(), Nat.N1(), 0.0, 0.0)
-        );
-    }
-
     private String id;
     
     private Translation2d offset;
@@ -83,7 +49,7 @@ public class SimModule extends AbstractSwerveModule {
         this.angleController = new AnglePIDController(Turn.kP, Turn.kI, Turn.kD)
             .setSetpointFilter(new ARateLimit(Swerve.MAX_TURNING));
 
-        this.driveSim = new LinearSystemSim<>(identifyVelocityPositionSystem(Drive.kV.get(), Drive.kA.get()));
+        this.driveSim = new PositionVelocitySystem(Drive.kV, Drive.kA).getSim();
         this.turnSim = new LinearSystemSim<N2,N1,N1>(LinearSystemId.identifyPositionSystem(Turn.kV.get(), Turn.kA.get()));
     }
 
---
+++ src/main/java/com/stuypulse/robot/util/PositionVelocitySystem.java
@@ -0,0 +1,69 @@
+package com.stuypulse.robot.util;
+
+import com.stuypulse.stuylib.network.SmartNumber;
+
+import edu.wpi.first.math.MatBuilder;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.wpilibj.simulation.LinearSystemSim;
+
+public class PositionVelocitySystem {
+    private double kV;
+    private double kA;
+
+    private LinearSystem<N2, N1, N2> system;
+    private LinearSystemSim<N2, N1, N2> sim;
+
+    /**
+     * Create a state-space model for a 1 DOF position system from its kV (volts/(unit/sec)) and kA (volts/(unit/sec²). 
+     * <li> States: [position, velocity]ᵀ 
+     * <li> Inputs  [voltage]
+     * <li> Outputs [position, velocity]
+     * 
+     * <p>The distance unit MUST either meters or radians.
+     * <p>The parameters provided by the user are from this feedforward model:
+     * <p>u = K_v v + K_a a
+     *
+     * @param kV The velocity gain, in volts/(unit/sec)
+     * @param kA The acceleration gain, in volts/(unit/sec²)
+     * @throws IllegalArgumentException if kV &lt;= 0 or kA &lt;= 0.
+     */
+    public PositionVelocitySystem(SmartNumber kV, SmartNumber kA) {
+        if (kV.get() <= 0.0) {
+            throw new IllegalArgumentException("kV must greater than zero");
+        }
+        if (kA.get() <= 0.0) {
+            throw new IllegalArgumentException("kA must be greater than zero");
+        }
+
+        this.kV = kV.get();
+        this.kA = kA.get();
+
+        this.system = new LinearSystem<N2, N1, N2> (
+            MatBuilder.fill(Nat.N2(), Nat.N2(), 0.0, 1.0, 0.0, -this.kV / this.kA),
+            MatBuilder.fill(Nat.N2(), Nat.N1(), 0.0, 1.0 / this.kA),
+            MatBuilder.fill(Nat.N2(), Nat.N2(), 1.0, 0.0, 0.0, 1.0),
+            MatBuilder.fill(Nat.N2(), Nat.N1(), 0.0, 0.0)
+        );
+
+        this.sim = new LinearSystemSim<N2, N1, N2>(this.system);
+    }
+
+    /**
+     * Gets the linear system.
+     * @return A LinearSystem representing the given characterized constants.
+     */
+    public LinearSystem<N2, N1, N2> getSystem() {
+        return this.system;
+    }
+
+    /**
+     * Gets the simulation of the system.
+     * @return A LinearSystemSim representing the simulation of the system.
+     */
+    public LinearSystemSim<N2, N1, N2> getSim() {
+        return this.sim;
+    }
+}
