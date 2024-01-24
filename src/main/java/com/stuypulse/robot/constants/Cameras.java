5aca825cebc3866bce99438192ad3ad8bec1868b
Ivan Chen <ivanchen07@gmail.com>
2024-01-24 06:44:16 -0500

    Fix nullPointerException with placeholder cameras

--- src/main/java/com/stuypulse/robot/constants/Cameras.java
+++ src/main/java/com/stuypulse/robot/constants/Cameras.java
@@ -21,8 +21,8 @@ public interface Cameras {
     }
     
     public CameraConfig[] APRILTAG_CAMERAS = new CameraConfig[] {
-        new CameraConfig(null, null),
-        new CameraConfig(null, null)
+        new CameraConfig("samera0", new Pose3d(new Translation3d(), new Rotation3d())),
+        new CameraConfig("samera1", new Pose3d(new Translation3d(), new Rotation3d()))
     };
     
     public static class CameraConfig {
