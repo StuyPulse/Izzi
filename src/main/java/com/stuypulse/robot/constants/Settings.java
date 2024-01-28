9e670da7d8aa0fd73095956d6b3a659115d94488
BenG49 <ben@goldfisher.com>
2024-01-27 22:56:01 -0500

    Update conveyor logic, constant naming, organization

--- src/main/java/com/stuypulse/robot/constants/Settings.java
+++ src/main/java/com/stuypulse/robot/constants/Settings.java
@@ -224,8 +224,9 @@ public interface Settings {
   
     public interface Conveyor {
         SmartNumber GANDALF_SHOOTER_SPEED = new SmartNumber("Conveyor/Gandalf Shooter Speed", 1);
-        SmartNumber GANDALF_AMP_SPEED = new SmartNumber("Conveyor/Gandalf Amp Speed", -1);
-        SmartNumber SHOOTER_FEEDER_SPEED = new SmartNumber("Conveyor/Shooter Feeder Speed", 1);
+        SmartNumber GANDALF_AMP_SPEED = new SmartNumber("Conveyor/Gandalf Amp Speed", 1);
+        SmartNumber FEEDER_SHOOTER_SPEED = new SmartNumber("Conveyor/Shooter Feeder Speed", 1);
+        SmartNumber FEEDER_AMP_SPEED = new SmartNumber("Conveyor/Shooter Feeder Speed", 1);
         
         SmartNumber DEBOUNCE_TIME = new SmartNumber("Conveyor/Debounce Time", 0.2);
     }
