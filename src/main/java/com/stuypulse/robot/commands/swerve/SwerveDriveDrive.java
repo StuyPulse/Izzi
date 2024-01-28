b1e6b6457af40e11276d8642b24eb123efd88d67
BenG49 <ben@goldfisher.com>
2024-01-27 21:44:13 -0500

    Remove gyro feedback

--- src/main/java/com/stuypulse/robot/commands/swerve/SwerveDriveDrive.java
+++ src/main/java/com/stuypulse/robot/commands/swerve/SwerveDriveDrive.java
@@ -1,13 +1,10 @@
 package com.stuypulse.robot.commands.swerve;
 
-import java.util.Optional;
-
 import com.stuypulse.robot.constants.Settings;
 import com.stuypulse.robot.constants.Settings.Driver.Drive;
 import com.stuypulse.robot.constants.Settings.Driver.Turn;
 import com.stuypulse.robot.subsystems.swerve.SwerveDrive;
 import com.stuypulse.stuylib.input.Gamepad;
-import com.stuypulse.stuylib.math.Angle;
 import com.stuypulse.stuylib.math.SLMath;
 import com.stuypulse.stuylib.streams.numbers.IStream;
 import com.stuypulse.stuylib.streams.numbers.filters.RateLimit;
@@ -16,7 +13,6 @@ import com.stuypulse.stuylib.streams.vectors.filters.VDeadZone;
 import com.stuypulse.stuylib.streams.vectors.filters.VLowPassFilter;
 import com.stuypulse.stuylib.streams.vectors.filters.VRateLimit;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class SwerveDriveDrive extends Command {
@@ -27,8 +23,6 @@ public class SwerveDriveDrive extends Command {
     private IStream turn;
 
     private final Gamepad driver;
-
-    private Optional<Rotation2d> holdAngle;
 
     public SwerveDriveDrive(Gamepad driver) {
         this.driver = driver;
@@ -47,37 +41,17 @@ public class SwerveDriveDrive extends Command {
 
         turn = IStream.create(driver::getRightX)
             .filtered(
-          //      x -> SLMath.deadband(x, Turn.DEADBAND.get()),
-          //      x -> SLMath.spow(x, Turn.POWER.get()),
-                x -> x * Turn.MAX_TELEOP_TURNING.get()
-          //      new RateLimit(Turn.RC.get())
+                x -> SLMath.deadband(x, Turn.DEADBAND.get()),
+                x -> SLMath.spow(x, Turn.POWER.get()),
+                x -> x * Turn.MAX_TELEOP_TURNING.get(),
+                new RateLimit(Turn.RC.get())
         ); 
-
-        holdAngle = Optional.empty();
         
         addRequirements(swerve);
     }
 
     @Override
-    public void initialize() {
-        holdAngle = Optional.empty();
-    }
-
-    private boolean isWithinTurnDeadband() {
-        return Math.abs(turn.get()) < Turn.DEADBAND.get();
-    }
-
-    @Override
     public void execute() {
-        double angularVel = turn.get();
-
-        if(isWithinTurnDeadband() && holdAngle.isEmpty()){
-            holdAngle = Optional.of(swerve.getGyroAngle());
-        }
-        else {
-            holdAngle = Optional.empty(); 
-        }
-
-        swerve.drive(speed.get(), angularVel);
+        swerve.drive(speed.get(), turn.get());
     }
 }
