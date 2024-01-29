481aca244158c42f51030ade3172913746fbb50f
BenG49 <ben@goldfisher.com>
2024-01-28 21:57:48 -0500

    Shooter cleanup

--- src/main/java/com/stuypulse/robot/subsystems/shooter/ShooterImpl.java
+++ src/main/java/com/stuypulse/robot/subsystems/shooter/ShooterImpl.java
@@ -14,6 +14,7 @@ import com.stuypulse.stuylib.control.feedforward.MotorFeedforward;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ShooterImpl extends Shooter {
+
     private final CANSparkMax leftMotor;
     private final CANSparkMax rightMotor;
 
@@ -27,16 +28,16 @@ public class ShooterImpl extends Shooter {
         leftMotor = new CANSparkMax(Ports.Shooter.LEFT_MOTOR, MotorType.kBrushless);
         rightMotor = new CANSparkMax(Ports.Shooter.RIGHT_MOTOR, MotorType.kBrushless);
 
+        Motors.Shooter.LEFT_SHOOTER.configure(leftMotor);
+        Motors.Shooter.RIGHT_SHOOTER.configure(rightMotor);
+
         leftEncoder = leftMotor.getEncoder();
         rightEncoder = rightMotor.getEncoder();
 
         leftController = new MotorFeedforward(Feedforward.kS, Feedforward.kV, Feedforward.kA).velocity()
             .add(new PIDController(PID.kP, PID.kI, PID.kD));
         rightController = new MotorFeedforward(Feedforward.kS, Feedforward.kV, Feedforward.kA).velocity()
-            .add(new PIDController(PID.kP, PID.kI, PID.kD));  
-
-        Motors.Shooter.LEFT_SHOOTER.configure(leftMotor);
-        Motors.Shooter.RIGHT_SHOOTER.configure(rightMotor);
+            .add(new PIDController(PID.kP, PID.kI, PID.kD));
     }
 
     @Override
@@ -68,8 +69,7 @@ public class ShooterImpl extends Shooter {
         SmartDashboard.putNumber("Shooter/Right RPM", getRightShooterRPM());
         SmartDashboard.putNumber("Shooter/Left RPM", getLeftShooterRPM());
 
-        SmartDashboard.putNumber("Shooter/Left Voltage", leftMotor.getBusVoltage()*leftMotor.getAppliedOutput());
-        SmartDashboard.putNumber("Shooter/Right Voltage", rightMotor.getBusVoltage()*rightMotor.getAppliedOutput());
+        SmartDashboard.putNumber("Shooter/Left Voltage", leftMotor.getBusVoltage() * leftMotor.getAppliedOutput());
+        SmartDashboard.putNumber("Shooter/Right Voltage", rightMotor.getBusVoltage() * rightMotor.getAppliedOutput());
     }
-}                                
-
+}
