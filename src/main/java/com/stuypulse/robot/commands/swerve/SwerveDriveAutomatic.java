9ff4815c153dea919aeab57bb01d0707f07d209d
ambers7 <ambrshen@gmail.com>
2024-02-02 17:13:15 -0500

    Add facing the amp if note is in amper

--- src/main/java/com/stuypulse/robot/commands/swerve/SwerveDriveAutomatic.java
+++ src/main/java/com/stuypulse/robot/commands/swerve/SwerveDriveAutomatic.java
@@ -3,7 +3,6 @@ package com.stuypulse.robot.commands.swerve;
 import com.stuypulse.robot.constants.Field;
 import com.stuypulse.robot.constants.Settings;
 import com.stuypulse.robot.constants.Settings.Swerve.*;
-import com.stuypulse.robot.constants.Settings.Alignment.Rotation;
 import com.stuypulse.robot.constants.Settings.Driver.Drive;
 import com.stuypulse.robot.subsystems.conveyor.Conveyor;
 import com.stuypulse.robot.subsystems.odometry.Odometry;
@@ -23,27 +22,9 @@ import com.stuypulse.stuylib.math.Angle;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
-/*
- * when a button is pressed this command is activated
- * point at either speaker or note (can add in pointing at amp later)
- * - use SwerveDriveToShoot command to point at speaker 
- * - pointing logic
- * -first check if there is a note in the speaker, intake, or amp 
- * --if so, point at the speaker 
- * -second check if there is a note (hasNoteData from LLNoteVision.java)
- * --if so, point at the note
- * --idt theres a function to point at the note right now but vision will probably add it in so we can j add aplaceholder or smth
- * -if there isnt a note nearby then just point at the speaker 
- * 
- * inside isFinisehd commanbd (stops if this is true)
- * - check if the joystick is moving (out of a certain range), or if a button is pressed
- * 
- * later: 
- * - try it out in a sim 
- * - in robotcontainer: bind a button to start the SwerveDriveAutomatic 
- * 
- */
+
 public class SwerveDriveAutomatic extends Command {
     
     private final SwerveDrive swerve;
@@ -89,11 +70,18 @@ public class SwerveDriveAutomatic extends Command {
         Translation2d currentPose = Odometry.getInstance().getPose().getTranslation();
         Rotation2d currentAngle = currentPose.getAngle();
         Rotation2d targetAngle;
-        Translation2d difference;
         Translation2d targetPose = getTargetPose();
-            
-        difference = targetPose.minus(currentPose);
-        targetAngle = difference.getAngle();
+
+        if (amper.hasNote()) { //if note in amp face wall 
+            if (DriverStation.getAlliance().get() == DriverStation.Alliance.Blue) { 
+                targetAngle = new Rotation2d(Math.PI); //180 degrees
+            } else { //on red 
+                targetAngle = new Rotation2d(0);
+            }
+        } else { //note not in amp 
+            Translation2d difference = targetPose.minus(currentPose);
+            targetAngle = difference.getAngle();
+        }
 
         controller.update(Angle.fromDegrees(targetAngle.getDegrees()), Angle.fromDegrees(currentAngle.getDegrees()));
         swerve.drive(drive.get(), controller.getOutput());
@@ -110,7 +98,7 @@ public class SwerveDriveAutomatic extends Command {
         if ((intake.hasNote() || conveyor.isNoteAtShooter()) && 
             (distanceToSpeaker<Assist.minDistToSPeaker.getAsDouble())) {
             targetPose = speakerPose;
-        }  else { // if (llNoteVision.hasNoteData()) {
+        } else { // if (llNoteVision.hasNoteData()) {
             targetPose = llNoteVision.getEstimatedNotePose();
         }
         return targetPose;
