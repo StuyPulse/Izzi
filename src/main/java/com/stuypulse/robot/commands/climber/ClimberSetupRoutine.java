de232a8792d1e01777eea76ecf14caec3e2fd3a0
Keobkeig <angela.xue3@gmail.com>
2024-02-06 21:55:08 -0500

    add ClimberSetupRoutine

--- src/main/java/com/stuypulse/robot/commands/climber/ClimberSetupRoutine.java
+++ src/main/java/com/stuypulse/robot/commands/climber/ClimberSetupRoutine.java
@@ -1,8 +1,19 @@
 package com.stuypulse.robot.commands.climber;
-
+import com.stuypulse.robot.commands.amper.AmperToHeight;
+import com.stuypulse.robot.commands.swerve.SwerveDriveDrive;
+import com.stuypulse.robot.constants.Ports;
+import com.stuypulse.robot.constants.Settings;
+import com.stuypulse.stuylib.input.Gamepad;
+import com.stuypulse.stuylib.input.gamepads.AutoGamepad;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-
-//TODO: Replace with Edgar's code
 public class ClimberSetupRoutine extends SequentialCommandGroup {
-    
+    public final Gamepad driver = new AutoGamepad(Ports.Gamepad.DRIVER);
+    public ClimberSetupRoutine() {
+        addCommands(
+            new AmperToHeight(Settings.Amper.Lift.MIN_HEIGHT),
+            new ClimberToTop(),
+            new SwerveDriveDrive(driver),
+            new ClimberToBottom()
+        );
+    }
 }
