7832d7f9f1078d3506d5c8f41fa8ecefb2756311
BenG49 <ben@goldfisher.com>
2024-02-09 15:33:11 -0500

    h

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -1,5 +1,3 @@
-
-
 /************************ PROJECT PHIL ************************/
 /* Copyright (c) 2024 StuyPulse Robotics. All rights reserved.*/
 /* This work is licensed under the terms of the MIT license.  */
