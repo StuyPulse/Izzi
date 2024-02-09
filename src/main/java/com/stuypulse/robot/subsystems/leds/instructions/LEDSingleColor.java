dc074cb3750fd9fbdacd8447558b12271b8115e1
BenG49 <ben@goldfisher.com>
2024-02-08 23:53:15 -0500

    Add LEDSingleColor constructors

--- src/main/java/com/stuypulse/robot/subsystems/leds/instructions/LEDSingleColor.java
+++ src/main/java/com/stuypulse/robot/subsystems/leds/instructions/LEDSingleColor.java
@@ -1,13 +1,25 @@
 package com.stuypulse.robot.subsystems.leds.instructions;
 
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+
 import com.stuypulse.robot.util.SLColor;
 
 public class LEDSingleColor implements LEDInstruction {
-    SLColor color;
+
+    private final SLColor color;
 
     public LEDSingleColor(SLColor color) {
         this.color = color;
+    }
+
+    public LEDSingleColor(Color color) {
+        this(new SLColor(color));
+    }
+
+    public LEDSingleColor(Color8Bit color) {
+        this(new SLColor(color));
     }
 
     @Override
