9e2e507fed59c762c108b3cf2e85ba58811829c0
Keobkeig <angela.xue3@gmail.com>
2024-02-10 10:20:24 -0500

    Add AmperLiftFineAdjust

---
+++ src/main/java/com/stuypulse/robot/commands/amper/AmperLiftFineAdjust.java
@@ -0,0 +1,29 @@
+package com.stuypulse.robot.commands.amper;
+
+import com.stuypulse.robot.subsystems.amper.Amper;
+import com.stuypulse.stuylib.input.Gamepad;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class AmperLiftFineAdjust extends Command {
+    private final Amper amper;
+    private final Gamepad gamepad;
+
+    public AmperLiftFineAdjust(Gamepad gamepad) {
+        amper = Amper.getInstance();
+        this.gamepad = gamepad;
+
+        addRequirements(amper);
+    }
+
+    @Override
+    public void execute() {
+        if (gamepad.getRawDPadUp()) {
+            amper.setTargetHeight(amper.getLiftHeight() + Units.inchesToMeters(1));
+        } 
+        else if (gamepad.getRawDPadUp()) {
+            amper.setTargetHeight(amper.getLiftHeight() - Units.inchesToMeters(1));
+        } 
+    }
+}
