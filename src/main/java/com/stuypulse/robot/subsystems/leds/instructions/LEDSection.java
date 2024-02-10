35b8fde098dac6eff193c7fd30cd16ad913c0c10
Material Energy <material.energitics@gmail.com>
2024-02-09 21:32:01 -0500

    reworked LEDSection
    to allow for custom lengthed sections of color. additionally, indexes can be omitted and they will be evenly distributed.

--- src/main/java/com/stuypulse/robot/subsystems/leds/instructions/LEDSection.java
+++ src/main/java/com/stuypulse/robot/subsystems/leds/instructions/LEDSection.java
@@ -1,30 +1,55 @@
 package com.stuypulse.robot.subsystems.leds.instructions;
 
+import com.stuypulse.robot.constants.Settings;
 import com.stuypulse.robot.util.SLColor;
 
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 
 public class LEDSection implements LEDInstruction {
-
     public SLColor[] sections;
+    public int[] indexes;
 
-    public LEDSection(SLColor[] sections) {
+
+    public LEDSection(SLColor[] sections, int[] indexes) {
+        if (sections.length != indexes.length) {
+            throw new IllegalArgumentException("Mismatching section and length indexes");
+        }
         this.sections = sections;
+        this.indexes = indexes;
     }
-        
+
+    public LEDSection(SLColor[] sections){
+        this.sections = sections;
+
+        int totalLEDLength = Settings.LED.LED_LENGTH;
+        int sectionLength = totalLEDLength / sections.length;
+        int extraLEDS = totalLEDLength % sections.length;
+
+        this.indexes = new int[sections.length];
+
+        //extra LEDs get distributed to the first few sections
+        for (int i = 0; i < sections.length; i++){
+            int offset = Math.min(extraLEDS, i);
+            int extraLEDForCurrentSection = (i < extraLEDS ? 1 : 0);
+            this.indexes[i] = (i + 1) * sectionLength + offset + extraLEDForCurrentSection;
+        }
+    }
+
     @Override
-    public void setLED(AddressableLEDBuffer ledsBuffer) { 
-        int sectionLength = ledsBuffer.getLength() / sections.length;
-        int extraLEDs = ledsBuffer.getLength() % sections.length;
-
-        int ledOffset = 0;
-
-        for (int i = 0; i < sections.length; i++) {
-            boolean appendLED = i + 1 <= extraLEDs;
-            for (int j = 0; j < sectionLength + (appendLED ? 1 : 0); j++) {
-                ledsBuffer.setRGB(i * sectionLength + ledOffset + j, sections[i].getRed(), sections[i].getGreen(), sections[i].getBlue());
+    public void setLED(AddressableLEDBuffer ledBuffer){
+        int sectionIndex = 0;
+        
+        for (int ledIndex = 0; ledIndex < ledBuffer.getLength(); ledIndex++){
+            if (indexes[sectionIndex] <= ledIndex){
+                sectionIndex++;
             }
-            if (appendLED) ledOffset++;
-        }  
+
+            SLColor color = sections[sectionIndex];
+            ledBuffer.setRGB(ledIndex,
+                color.getRed(),
+                color.getGreen(),
+                color.getBlue()
+            );
+        }
     }
 }
