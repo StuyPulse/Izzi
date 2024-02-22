bcdbe7acb792649c54f25757a628fb89a2318044
Material Energy <material.energitics@gmail.com>
2024-02-22 14:08:29 -0500

    \

--- src/main/java/com/stuypulse/robot/subsystems/leds/instructions/LEDSection.java
+++ src/main/java/com/stuypulse/robot/subsystems/leds/instructions/LEDSection.java
@@ -28,27 +28,17 @@ public class LEDSection implements LEDInstruction {
     public LEDSection(SLColor[] sections) {
         this.sections = sections;
 
-        int totalLEDLength = Settings.LED.LED_LENGTH; // 15
-        int sectionLength = totalLEDLength / sections.length; // 15 / 10 = 1
-        int extraLEDS = totalLEDLength % sections.length; // 15 % 10 = 5
+        int totalLEDLength = Settings.LED.LED_LENGTH;
+        int sectionLength = totalLEDLength / sections.length; 
+        int extraLEDS = totalLEDLength % sections.length;
 
-        this.separatorIndexes = new int[sections.length]; // int[10]
+        this.separatorIndexes = new int[sections.length];
 
         // extra LEDs get distributed to the first few sections
-        for (int i = 0; i < sections.length; i++) { // 0 - 9
+        for (int i = 0; i < sections.length; i++) {
             int offset = Math.min(extraLEDS, i); 
             int extraLEDForCurrentSection = (i < extraLEDS ? 1 : 0);
             this.separatorIndexes[i] = (i + 1) * sectionLength + offset + extraLEDForCurrentSection - 1;
-            // (0 + 1) * 1 + 0 + 1 - 1 = 1
-            // (1 + 1) * 1 + 1 + 1 - 1 = 3
-            // (2 + 1) * 1 + 2 + 1 - 1 = 5
-            // (3 + 1) * 1 + 3 + 1 - 1 = 7
-            // (4 + 1) * 1 + 4 + 1 - 1 = 9
-            // (5 + 1) * 1 + 5 + 0 - 1 = 10
-            // (6 + 1) * 1 + 5 + 0 - 1 = 11
-            // (7 + 1) * 1 + 5 + 0 - 1 = 12
-            // (8 + 1) * 1 + 5 + 0 - 1 = 13
-            // (9 + 1) * 1 + 5 + 0 - 1 = 14
         } 
     }
 
@@ -56,8 +46,7 @@ public class LEDSection implements LEDInstruction {
     public void setLED(AddressableLEDBuffer ledBuffer) {
         int sectionIndex = 0;
 
-        for (int ledIndex = 0; ledIndex < ledBuffer.getLength(); ledIndex++) { // 0 - 14
-
+        for (int ledIndex = 0; ledIndex < ledBuffer.getLength(); ledIndex++) {
             ledBuffer.setLED(ledIndex, sections[sectionIndex].getColor8Bit());
 
             if (ledIndex == separatorIndexes[sectionIndex]) {
