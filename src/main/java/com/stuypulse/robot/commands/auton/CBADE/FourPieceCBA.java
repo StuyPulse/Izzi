d78523a3e7e3ab7e63a9c491a84589a62ae38067
BenG49 <ben@goldfisher.com>
2024-02-22 21:06:48 -0500

    Add shooter startup delay

--- src/main/java/com/stuypulse/robot/commands/auton/CBADE/FourPieceCBA.java
+++ src/main/java/com/stuypulse/robot/commands/auton/CBADE/FourPieceCBA.java
@@ -4,17 +4,20 @@ import com.stuypulse.robot.commands.conveyor.ConveyorShootRoutine;
 import com.stuypulse.robot.commands.intake.IntakeAcquire;
 import com.stuypulse.robot.commands.shooter.ShooterPodiumShot;
 import com.stuypulse.robot.commands.swerve.SwerveDriveToShoot;
+import com.stuypulse.robot.constants.Settings.Auton;
 import com.stuypulse.robot.subsystems.swerve.SwerveDrive;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class FourPieceCBA extends SequentialCommandGroup {
 
     public FourPieceCBA() {
         addCommands(
             new ParallelCommandGroup(
-                new ShooterPodiumShot(),
+                new WaitCommand(Auton.SHOOTER_STARTUP_DELAY)
+                    .andThen(new ShooterPodiumShot()),
                 new SwerveDriveToShoot()
             ),
             new ConveyorShootRoutine(),
