17cb783cb4a78120dba441c7f0950e8204570015
Ivan Chen <ivanchen07@gmail.com>
2024-02-24 12:02:09 -0500

    Stop swerve after intake interrupts path

--- src/main/java/com/stuypulse/robot/commands/auton/FollowPathAndIntake.java
+++ src/main/java/com/stuypulse/robot/commands/auton/FollowPathAndIntake.java
@@ -4,10 +4,12 @@ import com.stuypulse.robot.commands.intake.IntakeAcquire;
 import com.stuypulse.robot.constants.Settings.Auton;
 import com.stuypulse.robot.subsystems.swerve.SwerveDrive;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
-public class FollowPathAndIntake extends ParallelRaceGroup {
+public class FollowPathAndIntake extends SequentialCommandGroup {
 
     public FollowPathAndIntake(String path) {
         this(path, Auton.DEFAULT_INTAKE_TIMEOUT);
@@ -15,10 +17,14 @@ public class FollowPathAndIntake extends ParallelRaceGroup {
     
     public FollowPathAndIntake(String path, double intakeTimeout) {
         addCommands(
-            new IntakeAcquire(),
+            new ParallelRaceGroup(
+                new IntakeAcquire(),
 
-            SwerveDrive.getInstance().followPathCommand(path)
-                .andThen(new WaitCommand(intakeTimeout))
+                SwerveDrive.getInstance().followPathCommand(path)
+                    .andThen(new WaitCommand(intakeTimeout))
+            ),
+
+            new InstantCommand(() -> SwerveDrive.getInstance().stop())
         );
     }
 
