9a59e7a9c75eb7a44e06ac6a18a9cf458c6f7f63
Ivan Chen <ivanchen07@gmail.com>
2024-02-24 10:25:45 -0500

    Add vision enable and disable commands

---
+++ src/main/java/com/stuypulse/robot/commands/vision/VisionDisable.java
@@ -0,0 +1,12 @@
+package com.stuypulse.robot.commands.vision;
+
+import com.stuypulse.robot.subsystems.odometry.Odometry;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+public class VisionDisable extends InstantCommand {
+
+    public VisionDisable() {
+        Odometry.getInstance().setVisionEnabled(false);
+    }
+}
---
+++ src/main/java/com/stuypulse/robot/commands/vision/VisionEnable.java
@@ -0,0 +1,12 @@
+package com.stuypulse.robot.commands.vision;
+
+import com.stuypulse.robot.subsystems.odometry.Odometry;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+public class VisionEnable extends InstantCommand {
+
+    public VisionEnable() {
+        Odometry.getInstance().setVisionEnabled(true);
+    }
+}
