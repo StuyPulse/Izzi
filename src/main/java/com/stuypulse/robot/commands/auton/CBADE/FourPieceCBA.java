291bbe4efbb221f09b7f3063cf0d0872ed4dd8d0
BenG49 <ben@goldfisher.com>
2024-02-25 23:35:23 -0500

    Remove shooter stop

--- src/main/java/com/stuypulse/robot/commands/auton/CBADE/FourPieceCBA.java
+++ src/main/java/com/stuypulse/robot/commands/auton/CBADE/FourPieceCBA.java
@@ -38,9 +38,7 @@ public class FourPieceCBA extends SequentialCommandGroup {
             // TODO: reduce angle tolerance on last shot
             new FollowPathAndIntake("B To A"),
             new SwerveDriveToShoot(35),
-            new ConveyorShootRoutine(),
-
-            new ShooterStop()
+            new ConveyorShootRoutine()
         );
     }
     
