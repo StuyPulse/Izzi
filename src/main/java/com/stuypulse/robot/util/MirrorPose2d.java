faec9a5ce4ec26f3e78eb191b5e269fcc44a2fa9
BenG49 <ben@goldfisher.com>
2024-03-05 19:04:50 -0500

    Fix MirrorPose2d

--- src/main/java/com/stuypulse/robot/util/MirrorPose2d.java
+++ src/main/java/com/stuypulse/robot/util/MirrorPose2d.java
@@ -1,34 +1,18 @@
 package com.stuypulse.robot.util;
 
-import com.stuypulse.robot.Robot;
-import com.stuypulse.robot.constants.Field;
-
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class MirrorPose2d {
 
-    private final boolean isBlue;
-    private final Pose2d pose;
-    private final Pose2d opposite;
-    
-    public MirrorPose2d(Alliance alliance, Pose2d pose) {
-        this.pose = pose;
-        
-        isBlue = alliance == Alliance.Blue;
-        opposite = Field.transformToOppositeAlliance(pose);
+    private final MirrorTranslation2d translation;
+    private final MirrorRotation2d rotation;
+
+    public MirrorPose2d(MirrorTranslation2d translation, MirrorRotation2d rotation) {
+        this.translation = translation;
+        this.rotation = rotation;
     }
 
     public Pose2d get() {
-        return (isBlue == Robot.isBlue()) ? pose : opposite;
+        return new Pose2d(translation.get(), rotation.get());
     }
-
-    public Pose2d getRed() {
-        return isBlue ? opposite : pose;
-    }
-
-    public Pose2d getBlue() {
-        return isBlue ? pose : opposite;
-    }
-
 }
