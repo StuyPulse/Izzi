9f891276777be591b110f005d82b0163f0bb72cd
BenG49 <ben@goldfisher.com>
2024-03-10 18:56:11 -0400

    Fix trap lift max extension

--- src/main/java/com/stuypulse/robot/constants/Settings.java
+++ src/main/java/com/stuypulse/robot/constants/Settings.java
@@ -103,7 +103,7 @@ public interface Settings {
             double CARRIAGE_MASS = 10; // kg
 
             double MIN_HEIGHT = 0;
-            double MAX_HEIGHT = Units.inchesToMeters(14.75); // trap: 17.0 
+            double MAX_HEIGHT = Units.inchesToMeters(14.75); // trap: 27.0 
 
             double VISUALIZATION_MIN_LENGTH = 0.5;
             Rotation2d ANGLE_TO_GROUND = Rotation2d.fromDegrees(68.02);
