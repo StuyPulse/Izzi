c6cb5386245c657c500d1b1476f9c9177e5d04f7
Edgar Ganahl <ganahledgar@gmail.com>
2024-03-11 18:57:27 -0400

    Actually finished ferry autons

--- src/main/java/com/stuypulse/robot/commands/auton/tests/FerryPathsAuton.java
+++ src/main/java/com/stuypulse/robot/commands/auton/tests/FerryPathsAuton.java
@@ -2,7 +2,11 @@ package com.stuypulse.robot.commands.auton.tests;
 
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.stuypulse.robot.commands.auton.FollowPathAndIntake;
+import com.stuypulse.robot.commands.conveyor.ConveyorShoot;
+import com.stuypulse.robot.commands.conveyor.ConveyorShootRoutine;
 import com.stuypulse.robot.commands.shooter.ShooterPodiumShot;
+import com.stuypulse.robot.commands.swerve.SwerveDriveToPose;
+import com.stuypulse.robot.constants.Settings;
 import com.stuypulse.robot.constants.Settings.Auton;
 import com.stuypulse.robot.subsystems.swerve.SwerveDrive;
 
@@ -15,21 +19,41 @@ public class FerryPathsAuton extends SequentialCommandGroup {
     public FerryPathsAuton(PathPlannerPath... paths) {
         addCommands(
 
+            new ParallelCommandGroup(
+                new WaitCommand(Auton.SHOOTER_STARTUP_DELAY)
+                    .andThen(new ShooterPodiumShot()),
+                
+                SwerveDriveToPose.speakerRelative(-45)
+            ),
+
             new WaitCommand(Auton.SHOOTER_STARTUP_DELAY),
 
             new FollowPathAndIntake(paths[0]),
+            new ConveyorShootRoutine(Settings.Conveyor.SHOOT_WAIT_DELAY.getAsDouble()),
             new ShooterPodiumShot(),
+
             new FollowPathAndIntake(paths[1]),
+            new ConveyorShootRoutine(Settings.Conveyor.SHOOT_WAIT_DELAY.getAsDouble()),
             new ShooterPodiumShot(),
+
             new FollowPathAndIntake(paths[2]),
+            new ConveyorShootRoutine(Settings.Conveyor.SHOOT_WAIT_DELAY.getAsDouble()),
             new ShooterPodiumShot(),
+
             new FollowPathAndIntake(paths[3]),
+            new ConveyorShootRoutine(Settings.Conveyor.SHOOT_WAIT_DELAY.getAsDouble()),
             new ShooterPodiumShot(),
+
             new FollowPathAndIntake(paths[4]),
+            new ConveyorShootRoutine(Settings.Conveyor.SHOOT_WAIT_DELAY.getAsDouble()),
             new ShooterPodiumShot(),
+
             new FollowPathAndIntake(paths[5]),
+            new ConveyorShootRoutine(Settings.Conveyor.SHOOT_WAIT_DELAY.getAsDouble()),
             new ShooterPodiumShot(),
+
             new FollowPathAndIntake(paths[6]),
+            new ConveyorShootRoutine(Settings.Conveyor.SHOOT_WAIT_DELAY.getAsDouble()),
             new ShooterPodiumShot()
 
         );
