0e8d367209401d7352ad8e141f4320192abf6487
Edgar Ganahl <ganahledgar@gmail.com>
2024-03-22 23:13:20 -0400

    6 Piece CBAED FIXED

--- src/main/java/com/stuypulse/robot/commands/auton/CBADE/SixPieceCBAED.java
+++ src/main/java/com/stuypulse/robot/commands/auton/CBADE/SixPieceCBAED.java
@@ -1,13 +1,12 @@
 package com.stuypulse.robot.commands.auton.CBADE;
 
 import com.pathplanner.lib.path.PathPlannerPath;
-import com.stuypulse.robot.commands.auton.FollowPathAlignAndShoot;
 import com.stuypulse.robot.commands.auton.FollowPathAndIntake;
 import com.stuypulse.robot.commands.conveyor.ConveyorShootRoutine;
 import com.stuypulse.robot.commands.shooter.ShooterPodiumShot;
 import com.stuypulse.robot.commands.swerve.SwerveDriveToPose;
-import com.stuypulse.robot.commands.swerve.SwerveDriveToShoot;
 import com.stuypulse.robot.constants.Settings.Auton;
+import com.stuypulse.robot.subsystems.swerve.SwerveDrive;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -40,12 +39,14 @@ public class SixPieceCBAED extends SequentialCommandGroup {
 
             new FollowPathAndIntake(paths[3]),
 
-            new FollowPathAlignAndShoot(paths[4], SwerveDriveToShoot.withHigherDebounce()),
+            SwerveDrive.getInstance().followPathCommand(paths[4]),
+            new ShooterPodiumShot(),
             new ConveyorShootRoutine(),
 
             new FollowPathAndIntake(paths[5]),
 
-            new FollowPathAlignAndShoot(paths[6], SwerveDriveToShoot.withHigherDebounce()),
+            SwerveDrive.getInstance().followPathCommand(paths[6]),
+            new ShooterPodiumShot(),
             new ConveyorShootRoutine()
 
         );
