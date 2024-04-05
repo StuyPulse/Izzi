785b7bded552a97ccdb7a443c527c3a5b3082b8b
Prog694 <prog692@gmail.com>
2024-04-05 16:49:34 -0400

    Remove some auton chooser

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -340,14 +340,11 @@ public class RobotContainer {
         // AutonConfig PodiumCloseCBAE = new AutonConfig("Podium Close 5 Piece CBAE", FivePiecePodiumForwardCBAE::new, 
         // "Forward First Piece to C", "C to B 2", "B to A","A to E", "E to Shoot");
         
+        // TODO: automatically choose red/blue
         HGF.registerDefaultBlue(autonChooser)
             .registerRed(autonChooser);
 
         CBAED
-            .registerBlue(autonChooser)
-            .registerRed(autonChooser);
-
-        CHGF
             .registerBlue(autonChooser)
             .registerRed(autonChooser);
         
