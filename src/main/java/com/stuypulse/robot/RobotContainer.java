9e34f0ffd4784b750f837381d98f172f202b3a86
Prog694 <prog692@gmail.com>
2024-06-22 09:39:31 -0500

    Remove note detection intaking

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -117,15 +117,13 @@ public class RobotContainer {
     private void configureDriverBindings() {
         // intaking
         driver.getRightTriggerButton()
-            .whileTrue(new SwerveDriveDriveToNote(driver))
+            // .whileTrue(new SwerveDriveDriveToNote(driver))
             .whileTrue(new IntakeAcquire()
                     .deadlineWith(new LEDSet(LEDInstructions.INTAKE))
-                    .andThen(new BuzzController(driver)));
-            // .whileTrue(new WaitUntilCommand(Intake.getInstance()::hasNote)
-            //     .deadlineWith(new LEDSet(LEDInstructions.INTAKE))
-            //     .andThen(new LEDSet(LEDInstructions.PICKUP)
-            //         .withTimeout(3.0)));
-        
+                .andThen(new BuzzController(driver) 
+                    .alongWith(new LEDSet(LEDInstructions.PICKUP)
+                        .withTimeout(3.0))));
+
         // intaking (also robot relative swerve)
         driver.getLeftTriggerButton()
             .whileTrue(new IntakeAcquire()
